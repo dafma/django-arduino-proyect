e65c845772629a726ec8ee54c34761bd8d630412
Makastin <makastin14@gmail.com>
2015-04-13 07:49:12 -0430

    chNGESSSS

--- tesis/tesis/settings.py
+++ tesis/tesis/settings.py
@@ -7,6 +7,9 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -37,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registro',
+    'home',
+    'sugerencia',    
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,11 +86,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
-
 TEMPLATE_CONTEXT_PROCESSORS = (
         
         'django.core.context_processors.request',
         'django.contrib.auth.context_processors.auth',
         
 )
+
+TEMPLATE_DIRS = (
+
+   os.path.join(PROJECT_ROOT, 'templates'),
+
+)
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    
+    os.path.join(PROJECT_ROOT,'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- tesis/tesis/urls.py
+++ tesis/tesis/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from yawdadmin import admin_site
+from django.conf import settings
+from django.contrib.auth.views import login, logout
 
 admin.autodiscover()
 admin_site._registry.update(admin.site._registry)
 
+
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin_site.urls)),
+        url(r'^admin/', include(admin_site.urls)),
+        url (r'^',include('home.urls')),
+        url(r'^login/$', login),
+		url(r'^logout/$', logout),
         
 )
 
