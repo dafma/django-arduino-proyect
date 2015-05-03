5a05bbc0679de0e6931d40606e3f7422feb7e054
Makastin <makastin@Pc>
2015-05-03 17:34:09 -0430

    changes

--- tesis/sugerencia/models.py
+++ tesis/sugerencia/models.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 class Sugerencia(models.Model): 
-    Usuiario = models.CharField(max_length=34)
+    Usuario = models.ForeignKey(User, unique=True)
     Sugerencia = models.TextField(blank=True)
 
     def __unicode__(self):
-        return self.Sugerencia
+        return '%s, coloco:  %s' %( self.Usuario, self.Sugerencia)
     
