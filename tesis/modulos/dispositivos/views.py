09ce3bc43fbadfcb44220480e015dfcbd2fdec66
urkh <gust989@gmail.com>
2015-05-26 13:38:39 -0430

    se actualiza lib para interactuar con arduino

--- tesis/modulos/dispositivos/views.py
+++ tesis/modulos/dispositivos/views.py
@@ -13,19 +13,21 @@ def dispositivos(request):
     if request.method == 'POST':
 
         # hago el import desde aqui, ya que sera la unica funcion en utilizar esta libreria
-        import serial
-        ser = serial.Serial('/dev/ttyACM0', 9600)
+        from Arduino import Arduino
+        board = Arduino('9600') #plugged in via USB, serial com at rate 9600
+        
         
         is_on = True
-        is_on_arduino = 'e'
+        is_on_arduino = 'HIGH'
 
         if request.POST['is_on'] == 'false':
             is_on = False
-            is_on_arduino = 'a'
+            is_on_arduino = 'LOW'
 
         dispositivo = Dispositivos.objects.filter(id=int(request.POST['id'])).update(is_on=is_on)
         puerto = Dispositivos.objects.get(id=dispositivo).puerto
-        ser.write(is_on_arduino)
+        board.pinMode(puerto, "OUTPUT")
+        board.digitalWrite(puerto, is_on_arduino)
 
         #import ipdb; ipdb.set_trace()
 
