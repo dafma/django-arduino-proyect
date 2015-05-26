b0128218f111b37cde216e7328f120eef1f36d38
urkh <gust989@gmail.com>
2015-05-26 11:06:09 -0430

    agregadas plantillas base

--- tesis/modulos/dispositivos/urls.py
+++ tesis/modulos/dispositivos/urls.py
@@ -5,5 +5,11 @@ urlpatterns = patterns(
 
     '',
     url(r'^$', 'modulos.dispositivos.views.resumen', name='resumen'),
+    url(r'^dispositivos/$', 'modulos.dispositivos.views.dispositivos', name='dispositivos'),
+    url(r'^tareas/$', 'modulos.dispositivos.views.tareas', name='tareas'),
+    url(r'^estadisticas/$', 'modulos.dispositivos.views.estadisticas', name='estadisticas'),
+    url(r'^reportes/$', 'modulos.dispositivos.views.reportes', name='reportes'),
+    url(r'^configuraciones/$', 'modulos.dispositivos.views.configuraciones', name='configuraciones'),
+
 
 )
--- tesis/modulos/dispositivos/views.py
+++ tesis/modulos/dispositivos/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def resumen(request):
-  return render_to_response('resumen/mainpage.html', context_instance=RequestContext(request))
+  return render_to_response('resumen/resumen.html', context_instance=RequestContext(request))
+
+
+def dispositivos(request):
+  return render_to_response('dispositivos/dispositivos.html', context_instance=RequestContext(request))
+
+
+def tareas(request):
+  return render_to_response('tareas/tareas.html', context_instance=RequestContext(request))
+
+
+def estadisticas(request):
+  return render_to_response('estadisticas/estadisticas.html', context_instance=RequestContext(request))
+
+
+def reportes(request):
+  return render_to_response('reportes/reportes.html', context_instance=RequestContext(request))
+
+
+def configuraciones(request):
+  return render_to_response('configuraciones/configuraciones.html', context_instance=RequestContext(request))
