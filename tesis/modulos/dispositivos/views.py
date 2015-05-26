efac99df3f36931b9ff290e5f1cad9892e2d76ca
urkh <gust989@gmail.com>
2015-05-26 13:57:58 -0430

    agregados comentarios

--- tesis/modulos/dispositivos/views.py
+++ tesis/modulos/dispositivos/views.py
@@ -10,24 +10,25 @@ def resumen(request):
 
 
 def dispositivos(request):
-    if request.method == 'POST':
+    if request.method == 'POST': # desde aqui recibo via ajax los datos enviados desde la plantilla (revisar archivo js en modulos/dispositivos/statis/js/dispositivos.js)
 
         # hago el import desde aqui, ya que sera la unica funcion en utilizar esta libreria
         from Arduino import Arduino
         board = Arduino('9600') #plugged in via USB, serial com at rate 9600
         
         
-        is_on = True
-        is_on_arduino = 'HIGH'
+        is_on = True # inicializo en True por si el parametro enviado llega a ser false (es decir, false = off)
+        is_on_arduino = 'HIGH' # inicializo en HIGH por si el parametro enviado llega a ser false (es decir, false = off)
 
-        if request.POST['is_on'] == 'false':
+        if request.POST['is_on'] == 'false': # si lo que le llega es false, entonces cambio las variables
             is_on = False
             is_on_arduino = 'LOW'
 
-        dispositivo = Dispositivos.objects.filter(id=int(request.POST['id'])).update(is_on=is_on)
-        puerto = Dispositivos.objects.get(id=dispositivo).puerto
-        board.pinMode(puerto, "OUTPUT")
-        board.digitalWrite(puerto, is_on_arduino)
+        dispositivo = Dispositivos.objects.filter(id=int(request.POST['id'])).update(is_on=is_on) #aqui actualizo en base de datos el estado del dispositivo
+        puerto = Dispositivos.objects.get(id=dispositivo).puerto # luego hago una busqueda para saber el numero del puerto de ese dispositivo y lo almaceno en la variable puerto
+        
+        board.pinMode(puerto, "OUTPUT") # les paso el numero del puerto
+        board.digitalWrite(puerto, is_on_arduino) # les paso el numero del puerto
 
         #import ipdb; ipdb.set_trace()
 
