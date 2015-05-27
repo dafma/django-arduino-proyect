b99b2e9c08fc5b87a99684384a81116dc02f123d
urkh <gust989@gmail.com>
2015-05-26 22:50:38 -0430

    se corrige bug

--- tesis/modulos/dispositivos/views.py
+++ tesis/modulos/dispositivos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 from modulos.dispositivos.models import Dispositivos
+from Arduino import Arduino
+board = Arduino('9600') 
 
 
 
@@ -10,12 +12,8 @@ def resumen(request):
 
 
 def dispositivos(request):
+    
     if request.method == 'POST': # desde aqui recibo via ajax los datos enviados desde la plantilla (revisar archivo js en modulos/dispositivos/statis/js/dispositivos.js)
-
-        # hago el import desde aqui, ya que sera la unica funcion en utilizar esta libreria
-        from Arduino import Arduino
-        board = Arduino('9600') #plugged in via USB, serial com at rate 9600
-        
         
         is_on = True # inicializo en True por si el parametro enviado llega a ser false (es decir, false = off)
         is_on_arduino = 'HIGH' # inicializo en HIGH por si el parametro enviado llega a ser false (es decir, false = off)
@@ -24,8 +22,10 @@ def dispositivos(request):
             is_on = False
             is_on_arduino = 'LOW'
 
-        dispositivo = Dispositivos.objects.filter(id=int(request.POST['id'])).update(is_on=is_on) #aqui actualizo en base de datos el estado del dispositivo
-        puerto = Dispositivos.objects.get(id=dispositivo).puerto # luego hago una busqueda para saber el numero del puerto de ese dispositivo y lo almaceno en la variable puerto
+        Dispositivos.objects.filter(id=int(request.POST['id'])).update(is_on=is_on) #aqui actualizo en base de datos el estado del dispositivo
+        puerto = Dispositivos.objects.get(id=int(request.POST['id'])).puerto # luego hago una busqueda para saber el numero del puerto de ese dispositivo y lo almaceno en la variable puerto
+        
+        #import ipdb; ipdb.set_trace()
         
         board.pinMode(puerto, "OUTPUT") # les paso el numero del puerto
         board.digitalWrite(puerto, is_on_arduino) # les paso el numero del puerto
