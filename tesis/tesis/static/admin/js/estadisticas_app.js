83100e64c4a2565bf41989f3ff1a742c58e96fa0
Dehivis Perez <dehivis.oliver@gmail.com>
2015-06-16 20:23:36 +0000

    "reparando la locura de marcos que no se quiere graduar"

---
+++ tesis/tesis/static/admin/js/estadisticas_app.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+
+    $.getJSON("/resumen/", function(data){
+
+        $('#chart1').highcharts({
+            chart: {
+                type: 'pie',
+                options3d: {
+                    enabled: true,
+                    alpha: 45,
+                    beta: 0
+                }
+            },
+            title: {
+                text: 'Dispositivo cantidad usado'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    depth: 35,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.name}'
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Porcentaje',
+                data: data 
+            }]
+        });
+
+    });
+
+
+    $.getJSON("resumen", function(data){
+
+        // Build the chart
+        $('#chart2').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'Dispositivo mas Demandado'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Browser share',
+                data: data
+            }]
+        });
+
+    });
+});
