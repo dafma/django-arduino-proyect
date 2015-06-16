d4b08d6d767a0f8df2e7bcecd2ac5a85d5042c21
Makastin <makastin@Pc>
2015-06-16 17:52:13 -0430

    graficas vistas

--- tesis/modulos/home/views.py
+++ tesis/modulos/home/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
 from django.core.context_processors import csrf
 from django.template import RequestContext
@@ -5,3 +7,13 @@ from django.shortcuts import render_to_response
 
 def vista_principal(request):
   return render_to_response('home/mainpage.html', context_instance=RequestContext(request))
+
+def usos_est(request):
+    query = UsosDisp.objects.all().values_list('actividad__actividad').annotate(total=Count('actividad')).order_by('total')
+    data = json.dumps(list(query), cls=DjangoJSONEncoder)
+    return HttpResponse(data, mimetype='application/json')
+
+def dispositivos_est(request):
+    query = Dispositivos.objects.all().values_list('medico__primer_nombre').annotate(total=Count('medico')).order_by('total')
+    data = json.dumps(list(query), cls=DjangoJSONEncoder)
+    return HttpResponse(data, mimetype='application/json')
